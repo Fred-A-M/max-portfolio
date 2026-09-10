4ae4c44509fe74e02dd2e33c77cd90ffed4a718a
Fred Moore <frederic_archer@hotmail.com>
2026-09-10 22:12:35 +1000

    testing mobile home

--- app/components/WorkFrameHomePage.tsx
+++ app/components/WorkFrameHomePage.tsx
@@ -1,13 +1,14 @@
 "use client"
 import Link from 'next/link';
 import { projects } from '../consts';
+import { useIsMobile } from '../hooks/useIsMobile';
 
 
 export default function WorkFrameHomePage () {
-  
+  const isMobile = useIsMobile();
 
 
-  return (
+  if (!isMobile) return (
     <nav className="absolute inset-0 z-0">
 
       <div className='absolute inset-[12px] '>
@@ -27,6 +28,39 @@ export default function WorkFrameHomePage () {
             THAT WAY
           </Link>
         </div>
+
+      </div>
+
+    </nav>
+  )
+
+  if (isMobile) return (
+    <nav className="absolute inset-0 z-10">
+
+      <div className='absolute inset-[12px]'>
+        {/* top bar */}
+        <div className="absolute top-0 left-0 right-0 flex flex-col justify-center gap-[25px] items-center">
+          <div className='w-full flex justify-center'>
+            <Link href="/" className="hover:underline hover:cursor-pointer">
+              
+            </Link>
+          </div>
+          <Link href={`/work/${projects[projects.length - 1].link}`} prefetch className='hover:cursor-pointer text-3xl hover:text-4xl'>
+            &uarr;
+          </Link>
+        </div>
+
+  
+      
+
+       <div className='absolute bottom-0 left-0 right-0 flex flex-col items-center justify-center gap-[25px]'>
+          <Link href={`/work/${projects[0].link}`} prefetch className='hover:cursor-pointer text-3xl hover:text-4xl'>
+            &darr;
+          </Link>
+          <div>
+            {" "}
+          </div>
+       </div>
 
       </div>
 
--- app/main.tsx
+++ app/main.tsx
@@ -71,47 +71,70 @@ export default function Main () {
               <Link href="https://www.instagram.com/handburgers/" className='hover:underline' target='_blank'>
                 @Handburgers
               </Link>
-          </div>
+            </div>
         </div>
-   
-  )}
+    ) 
+  }
 
   if (isMobile) {
     return (
-      <div className='flex flex-col flex-1 px-[12px] py-[12px]'>
-        <div className='flex'>
-          <div className='flex flex-col w-3/4 sm:flex-1'>
-            <div className='flex flex-col gap-[25px] pb-[25px]'>
-              <h1 className='underline'>
-              I&apos;M MAX RAWLINS. WELCOME TO MY WEBSITE.
-            </h1>
-            <p>
-              I am a graphic designer and art director currently working in London. I enjoy working on projects within music and apparel.
-            </p>
-            <p>
-              Experience working with and for:
-            </p>
-            <div className='flex flex-col'>
-              {profile.workedWith.map((brand, index) => (
-                <p key={index}>{brand}</p>
-              ))}
+      <div
+        className={`
+          relative
+          w-full
+          grid
+          grid-rows-[auto,1fr]
+          transition-opacity
+          duration-500
+          ${ready ? "opacity-100" : "opacity-0"}
+          `}
+          
+        >
+          <WorkFrameHomePage />
+         
+          <div 
+            className="
+              px-[12px]
+              py-[12px]
+              h-full
+              mx-auto
+              w-1/2
+              flex
+              flex-col
+              gap-[15px]
+              justify-center
+              text-left
+              z-0
+              "
+            >
+              <div className="min-h-0 min-w-0 flex items-center justify-center">
+                <CldImage
+                  src={profile.image}
+                  alt="Max Rawlins Logo"
+                  width={350}
+                  height={350}
+                  preload
+                  onLoad={() => setCounter(prev => prev + 1)}
+                  className="max-w-full max-h-full w-auto h-auto object-contain mb-[20px]"
+                />
+              </div>
+              <p>
+                {`Max Rawlins is a Designer and art director based in\u00A0London.`}
+              </p>
+              <p>
+                {`Clients include: Adidas, All Purpose, Awake Ny, Ballantines, Basement, Boiler Room, Budweiser, Dazed, Fiorucci, Levi's, MTV, New Balance, Nike,
+                Nowadays, Papa Johns, Superimpose, Umbro, Urban Outfitters, With\u00A0Projects.`}
+              </p>
+              <p>
+                Full portfolio available upon request.
+              </p>
+              <Link href="mailto:helloitsmaxrawlins@gmail.com" className='hover:underline'>
+                XXXXXXXXXX@gmail.com
+              </Link>
+              <Link href="https://www.instagram.com/handburgers/" className='hover:underline' target='_blank'>
+                @Handburgers
+              </Link>
             </div>
-          </div>
         </div>
-        <div className='flex-1 text-right'>
-          <Link href="/work/" className='hover:underline'>
-            WORK
-          </Link>
-        </div>
-      </div>
-      <div className='flex flex-col mt-auto'>
-        <Link href="mailto:helloitsmaxrawlins@gmail.com" className='hover:underline'>
-          EMAIL
-        </Link>
-        <Link href="https://www.instagram.com/handburgers/" className='hover:underline' target='_blank'>
-          INSTAGRAM
-        </Link>
-      </div>
-    </div>
-  )}
+    )}
 }
